002a6baf528d14554ad917e296fc323cde3ddd4d
Meghana-Veeramallu <veeramallumeghana@gmail.com>
2025-10-14 11:57:40 +0530

    fix: srcnn

--- srcnn.py
+++ srcnn.py
@@ -4,12 +4,13 @@ import torch.nn as nn
 class SRCNN(nn.Module):
     def __init__(self):
         super(SRCNN, self).__init__()
-        self.model = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=9, padding=4),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 32, kernel_size=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(32, 3, kernel_size=5, padding=2)
-        )
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=9, padding=4)
+        self.conv2 = nn.Conv2d(64, 32, kernel_size=5, padding=2)
+        self.conv3 = nn.Conv2d(32, 3, kernel_size=5, padding=2)
+        self.relu = nn.ReLU(inplace=True)
+    
     def forward(self, x):
-        return self.model(x)
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.conv3(x)
+        return x
